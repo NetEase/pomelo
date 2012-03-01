edcb44127762762bcb6ca974854883862875fdc9
xiecc <xieccy@gmail.com>
2012-03-01 17:54:42 +0800

    misc: run through server

--- demos/webrunner/app.js
+++ demos/webrunner/app.js
@@ -9,16 +9,16 @@ var args = process.argv;
 
 var env = 'development';
 
-if (args.length > 0){
-    env = args[0];
+if (args.length > 2){
+    env = args[2];
 }
 
 var serverType = 'all';  // if dev, means all server
 var serverId = '';
 
-if ((args.length == 3) && (env == 'production')){
-    serverType = args[1];  //  area, logic, login or other servers
-    servereId = args[2];
+if ((args.length == 4) && (env == 'production')){
+    serverType = args[2];  //  area, logic, login or other servers
+    servereId = args[3];
 }
 
 app.set('name', 'webrunner');
@@ -26,56 +26,55 @@ app.set('env', env);
 app.set('serverType', serverType);
 app.set('serverId', serverId);
 
+console.log('before app.configure');
+
+app.configure(function(){
+	  app.use(app.router); //filter out requests
+	  app.use(logFilter); //filter out requests
+	  app.set('scheduler', '../config/scheduler.coffee');
+	  app.enabled('scheduler');
+	  
+	   // 全部生成代理
+	  app.genRemote('../lib/connector/remote');
+	  
+	  //user proxy
+	  app.genHandler('./app/connector/handler');
+	  app.genRemote('./app/connector/remote');
+	  app.genHandler('./app/area/handler');
+	  app.genRemote('./app/area/remote');
+	  app.genHandler('./app/logic/handler');
+	  app.genRemote('./app/logic/remote');
+      
+      startWebServer();
+});
+
 // use is filter
 app.configure('development',function(){
-  app.set('servers', '../config/servers-development.json');
-  app.set('database','../config/database.json');
+  app.set('servers', __dirname+'/config/servers-development.json');
+  app.set('database',__dirname+'/config/database.json');
+  
+  app.listenAll(app.get('servers'));  // listenAll servers on certain port
 })
 
 app.configure('production',function(){
-  app.set('servers', '../config/servers-production.json');
-  app.set('database','../config/database.json');
+  app.set('servers', __dirname + '/config/servers-production.json');
+  app.set('database',__dirname + '/config/database.json');
+  
+  app.listen(app.serverType, app.serverId);  
 });
 
-app.configure(function(){
-  app.use(app.router); //filter out requests
-  app.use(logFilter); //filter out requests
-  app.set('scheduler', '../config/scheduler.coffee');
-  app.enabled('scheduler');
-  
-   // 全部生成代理
-  app.genRemote('../lib/connector/remote');
-  
-  //user proxy
-  app.genHandler('./app/connector/handler');
-  app.genRemote('./app/connector/remote');
-  app.genHandler('./app/area/handler');
-  app.genRemote('./app/area/remote');
-  app.genHandler('./app/logic/handler');
-  app.genRemote('./app/logic/remote');
-});
+
+//master run other servers 
+app.configure('production', 'master', function(){
+ app.runAll(app.get('servers'), 'master'); // run other servers except master
+})
 
 
 app.configure(function(){
   app.use(app.handlerManager); //the last handler
-  startWebServer();
 });
 
-// all listening
-app.configure('development', 'all', function(){
-    app.listenAll(app.servers);  // listenAll servers on certain port
-})
 
-// master run other servers 
-app.configure('production', 'master', function(){
-    app.listen(app.serverType, app.serverId);
-    app.runAll(app.servers, 'master'); // run other servers except master
-})
-
-//  node master, listen on port
-app.configure('production', '^master', function(){
-    app.listen(app.serverType, app.serverId);  
-})
 
 function startWebServer(){
     var app_express = require('./app_express');
