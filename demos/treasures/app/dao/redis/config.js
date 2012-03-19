bb6c4729b3f1b98e291fd8587f12dd70134cda19
piaohai <piaohai@gmail.com>
2012-03-19 18:10:24 +0800

    misc:edit client

--- demos/treasures/app/dao/redis/config.js
+++ demos/treasures/app/dao/redis/config.js
@@ -1,6 +1,6 @@
 var config = module.exports;
 
-config.host = '192.168.130.107'; //'172.31.133.110' vpn
+config.host = 'app47v2.photo.163.org'; //'172.31.133.110' vpn
 config.db = 'Pomelo';
 config.dbport = '3306';
 config.dbusername = 'xy';
