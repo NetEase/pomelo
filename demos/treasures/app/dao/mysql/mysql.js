274393e0dc149088d5e7e524526873fd28e0b68f
piaohai <piaohai@gmail.com>
2012-03-20 18:34:19 +0800

    misc:add mysql config

--- demos/treasures/app/dao/mysql/mysql.js
+++ demos/treasures/app/dao/mysql/mysql.js
@@ -1,11 +1,13 @@
 var client = require('./sql');
 client.init();
+
+var sqlclient = module.exports;
+
 sqlclient.insert = client.query;
 sqlclient.update = client.query;
 sqlclient.delete = client.query;
 sqlclient.query = client.query;
 
-var sqlclient = module.exports;
 
 
 
