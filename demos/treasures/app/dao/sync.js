5e0ea3e174da652348a0be275a193f8b29f04bf4
piaohai <piaohai@gmail.com>
2012-03-26 17:54:49 +0800

    misc:edit master

--- demos/treasures/app/dao/sync.js
+++ demos/treasures/app/dao/sync.js
@@ -6,6 +6,7 @@ var Mysqlrewriter = require('./mysqlrewriter');
 var opt = {};
 opt.client = dbclient;
 opt.rewriter = new Mysqlrewriter();
+opt.interval = 1000 * 10;
 
 var redis = new MemDatabase(this,opt) ;
 
