6dceb1a3c07536a46f623611f31c822bda7f1388
xiecc <xieccy@gmail.com>
2012-04-06 14:31:08 +0800

    misc: modfiy test path

---
+++ test/pomeloTest.js
@@ -0,0 +1,17 @@
+var pomelo = require('../lib/pomelo');
+var should = require('should');
+
+describe('pomelo', function(){
+    describe('#createApp', function(){
+        it('should create and get app, be the same instance', function(done){
+          var app = pomelo.createApp();  
+          should.exist(app);
+          
+          var app2 = pomelo.getApp();
+          should.exist(app2);
+          should.strictEqual(app, app2);
+          done();
+        });
+    });
+});
+	
