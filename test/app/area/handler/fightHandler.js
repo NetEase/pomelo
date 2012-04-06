217cf0e05887fadca914ca0ecfac2e896c5feef1
xiecc <xieccy@gmail.com>
2012-04-06 14:32:37 +0800

    misc: move integration test dir

--- test/app/area/handler/fightHandler.js
+++
@@ -1,9 +0,0 @@
-var utils = require('../../../../lib/util/utils');
-
-var handler = module.exports;
-
-handler.kick = function(session, cb){
-    var params = session.params;
-	console.log(' fight user: '+ params.sourceUsername+'  target: ' + params.targetUsername);
-    utils.invokeCallback(cb, null, 'kick ass in server sUsername: '+params.sourceUsername+ ' target: ' + params.targetUsername, 'iAttach');
-}
