5ed3b717a20b9006724e6a5e0a0adcb90bcff8ad
xiecc <xieccy@gmail.com>
2012-04-09 17:00:16 +0800

    misc: add some unit test

--- test/handlerManagerTest.js
+++ test/handlerManagerTest.js
@@ -5,23 +5,27 @@ var pomelo = require('../lib/pomelo');
 
 describe('handlerManagerTest', function(){
     var app = pomelo.createApp();
-      app.genHandler('area',  __dirname + '/config/area/handler');
+    
+    
+    before(function(done){
+        app.set('serverType', 'area');
+        app.genHandler('area',  __dirname + '/config/area/handler');
         app.genRemote('area',  __dirname + '/config/area/remote');
         app.genProxy('area',  __dirname + '/config/area/remote');
-    
-//    before(function(done){
-//        app.genHandler('area',  __dirname + '/config/area/handler');
-//        app.genRemote('area',  __dirname + '/config/area/remote');
-//        app.genProxy('area',  __dirname + '/config/area/remote');
-//        done();
-//    });
+        done();
+    });
     
     it('local handler should be ok!', function(done){
         app.serverType = 'all';
-        var msg = {route: 'area.userHandler.move', params:{}};
-        var session = {};
+        var msg = {route: 'area.userHandler.move', params:{uid: 1, x:1, y:2}};
+        var session = {uid:1, sessionId:1};
         
-        handlerManager.handle(msg, session);
+        handlerManager.handle(msg, session, function(error, msg){
+            if (!!error){
+            	console.log('[handlerManagerTest] error: ' + error.message);
+            }
+            should.not.exist(error);
+        });
         done();
     });
 });
