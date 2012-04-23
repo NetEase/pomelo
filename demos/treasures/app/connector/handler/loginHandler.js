123a683207375d0ac0b35eb0d1afbe825f8e77d1
xiecc <xieccy@gmail.com>
2012-04-23 16:17:45 +0800

    misc: add pomelo client library

--- demos/treasures/app/connector/handler/loginHandler.js
+++ demos/treasures/app/connector/handler/loginHandler.js
@@ -13,7 +13,7 @@ exp.login = function(msg, session) {
 			session({route: msg.route, code: 500});
 			return;
 		}
-		
+
 		logger.debug('[login] login success, uid:' + uinfo.uid);
 		afterLogin(msg, session, uinfo);
 	});
@@ -27,17 +27,17 @@ exp.register = function(msg, session) {
 			session.response({route: msg.route, code: 500, error:err});
 			return;
 		}
-		
+
 		logger.debug('[register] register success, uid:' + uinfo.uid);
 		afterLogin(msg, session, uinfo);
 	});
 };
 
 var afterLogin = function(msg, session, uinfo) {
-	logger.error('uinfo: %j', uinfo);
+	//logger.error('uinfo: %j', uinfo);
 	session.userLogined(uinfo.uid);
 	session.on('closing', onUserLeave);
-	
+
 	session.response({route: msg.route, code: 200, userData: uinfo});
 };
 
@@ -45,7 +45,7 @@ var onUserLeave = function(session) {
 	if(!session || !session.uid) {
 		return;
 	}
-	
+
 	pomelo.getApp().get('proxyMap').user.area.userService.userLeave(session.uid, function(err) {
 		//TODO: logout logic
 		//TODO: remember to call session.closed() to finish logout flow finally
