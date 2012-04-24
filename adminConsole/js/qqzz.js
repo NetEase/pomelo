09c08d2aac5817f4e4745574aa5f0ebac537f64a
lwj <lwj19861218@163.com>
2012-04-24 15:57:07 +0800

    monitor log and requestion list

--- adminConsole/js/qqzz.js
+++ adminConsole/js/qqzz.js
@@ -6,7 +6,7 @@ Ext.onReady(function(){
 	id:'reqStoreId',
 	autoLoad:false,
 	pageSize:5,
-    fields:['source','request route','request params','createTime','doneTime','cost(ms)'],
+    fields:['source','route','params','createTime','doneTime','cost(ms)'],
 //    groupField: 'department',
     proxy: {
         type: 'memory',
@@ -25,9 +25,10 @@ var reqGrid=Ext.create('Ext.grid.Panel', {
 	region:'center',
     store: reqStore,
     columns:[
-		{header:'source',dataIndex:'source',width:150},
-		{header:'request route',dataIndex:'request route',width:230},
-		{header:'request params',dataIndex:'request params',width:400}
+		// {header:'source',dataIndex:'source',width:150},
+		{xtype:'rownumberer',width:50,sortable:false},
+		{text:'request route',dataIndex:'route',width:230},
+		{text:'request params',dataIndex:'params',width:900}
 //		{header:'createTime',dataIndex:'createTime',width:200},
 //		{header:'doneTime',dataIndex:'doneTime',width:200},
 //		{header:'cost(ms)',dataIndex:'cost(ms)',width:800}
@@ -41,18 +42,12 @@ var viewport=new Ext.Viewport({
 });
 	socket.on('connect',function(){
 		socket.emit('announce_web_client');
-		socket.emit('webmessage',{id:'area-server-1',method:'getMessage'});
-		socket.on('webmessage',function(msg){
-	  	var dataMap=msg.body;
-	  	var dataGrid=[];
-	    for(var i in dataMap){
-	    	var content=JSON.stringify(dataMap[i]);
-	    	var data={'source':'area','request route':i,'request params':content};
-	       dataGrid.push(data)
-	       }
+		socket.emit('logMessage');
+		socket.on('logMessage',function(msg){
+	  	
 	      
 	   var store=Ext.getCmp('reqGridId').getStore();
-       store.loadData(dataGrid);
+       store.loadData(msg);
 	  });
 	});
 	
