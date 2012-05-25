d3b9fe7264e18ce12ffee495ebaf3095589a2fdb
yaopiaohai <yaopiaohai@corp.netease.com>
2012-05-25 18:44:25 +0800

    misc:add profiler done

--- adminConsole/js/profiler.js
+++ adminConsole/js/profiler.js
@@ -24,7 +24,7 @@ Ext
 
 			var scriptCom = Ext.create('Ext.form.ComboBox', {
 				id : 'scriptComId',
-				fieldLabel : 'profiler Type',
+				fieldLabel : '&nbsp;&nbsp;&nbsp;&nbsp;  Type',
 				labelWidth : 80,
 				store : scriptStore,
 				queryMode : 'local',
@@ -43,17 +43,10 @@ Ext
 					anchor : '95%',
 					items : [ {
 						xtype : 'label',
-						text : 'runScript:',
+						text : 'Profiler:',
 						columnWidth : .99
 					}, serverCom, scriptCom ]
-				}, {
-					xtype : 'textareafield',
-					height : 150,
-					id : 'scriptAreaId',
-					readOnly : true,
-					anchor : '95%'
-
-				}, {
+				},  {
 					layout : 'column',
 					anchor : '95%',
 					border : false,
@@ -65,7 +58,7 @@ Ext
 						// colspan: 2
 						xtype : 'button',
 						text : 'start',
-						handler : runScript,
+						handler : startProf,
 						columnWidth : 0.08
 					}, {
 						xtype : 'label',
@@ -75,7 +68,7 @@ Ext
 						// colspan: 2
 						xtype : 'button',
 						text : 'stop',
-						handler : save,
+						handler : stopProf,
 						columnWidth : 0.08
 					} ]
 				}, {
@@ -85,13 +78,9 @@ Ext
 				}, {
 					xtype : 'textareafield',
 					id : 'tesultTextId',
-					height : 150,
+					height : 550,
 					name : 'scriptId',
 					anchor : '95%'
-				}, {
-					id : 'runtimeId',
-					html : 'runtime:',
-					border : false
 				} ]
 			});
 
@@ -100,50 +89,8 @@ Ext
 				items : [ runScriptPanel ]
 			});
 		});
-function save() {
-	var saveForm = Ext.create('Ext.form.Panel', {
-		frame : true,
-		bodyStyle : 'padding:2px 2px 0',
-		width : 300,
-		// defaultType: 'textfield',
-		// renderTo: Ext.getBody(),
-		anchor : '100%',
-		fieldDefaults : {
-			msgTarget : 'side',
-			labelWidth : 50
-		},
-		items : [ {
-			xtype : 'textfield',
-			id : 'scriptNameId',
-			fieldLabel : 'name',
-			name : 'scriptName',
-			allowBlank : false,
-			width : 250,
-			value : Ext.getCmp('scriptComId').getValue()
-		} ],
-		buttons : [ {
-			text : 'Save',
-			handler : saveFile
-		}, {
-			text : 'Cancel',
-			handler : cancel
-		} ]
-	});
-	var win = Ext.create('Ext.window.Window', {
-		id : 'saveWinId',
-		title : 'saveScript',
-		height : 100,
-		width : 320,
-		layout : 'fit',
-		anchor : '100%',
-		items : [ saveForm ]
-	});
-	win.show()
-}
-function cancel() {
-	Ext.getCmp('saveWinId').close();
-}
-socket.on('connect', function() {
+
+	socket.on('connect', function() {
 	socket.emit('announce_web_client');
 	socket.emit('webMessage', {
 		method : 'getSer_Scr'
@@ -158,7 +105,7 @@ socket.on('connect', function() {
 	});
 });
 // run the cript
-function runScript() {
+function startProf() {
 	var scriptJs = Ext.getCmp('scriptComId').getValue();
 	var serverId = Ext.getCmp('serverComId').getValue();
 	if (!serverId || serverId.length < 1) {
@@ -171,41 +118,32 @@ function runScript() {
 	}
 	socket.emit('webMessage', {
 		method : 'profiler',
-		scriptJs : scriptJs,
+		type : scriptJs,
+            action:'start',
 		serverId : serverId
 	});
-	socket.on('runScript', function(msg) {
+	socket.on('profiler', function(msg) {
 		Ext.getCmp('tesultTextId').setValue(msg);
 	});
 }
-var getFile = function(filename) {
-	socket.emit('webMessage', {
-		method : 'getFile',
-		filename : filename
-	});
-	socket.on('getFile', function(msg) {
-		Ext.getCmp('scriptAreaId').setValue(msg);
-	})
-}
-var saveFile = function() {
-	var filename = Ext.getCmp('scriptNameId').getValue();
-	if (!filename.match(/\.js$/)) {
-		alert('the filename is required!');
+
+// run the cript
+function stopProf() {
+	var scriptJs = Ext.getCmp('scriptComId').getValue();
+	var serverId = Ext.getCmp('serverComId').getValue();
+	if (!serverId || serverId.length < 1) {
+		alert('serverId is required!');
 		return;
 	}
-	var data = Ext.getCmp('scriptAreaId').getValue();
+	if (!scriptJs || scriptJs.length < 1) {
+		alert('profiler type is required!');
+		return;
+	}
 	socket.emit('webMessage', {
-		method : 'saveFile',
-		filename : filename,
-		data : data
-	});
-	socket.on('saveFile', function(msg) {
-		if (msg == 'yes') {
-			Ext.getCmp('saveWinId').close();
-			alert('save success!');
-			data = Ext.getCmp('scriptAreaId').setValue('');
-		} else {
-			alert('save failed!');
-		}
+		method : 'profiler',
+		type : scriptJs,
+            action:'stop',
+		serverId : serverId
 	});
 }
+
