b1433419392e7ddd98ea05b3fd337c6418f73ea8
py <hzpengyang@corp.netease.com>
2012-11-26 14:46:13 +0800

    update template code format
    Change-Id: Ic1032fcbefdf15e1f89ef58aa2a23b5d04f6a7e6

--- template/web-server/app.js
+++ template/web-server/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express.createServer();
 
 app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.set('view engine', 'jade');
-    app.set('views', __dirname + '/public');
-    app.set('view options', {layout: false});
-    app.set('basepath',__dirname + '/public');
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.set('view engine', 'jade');
+	app.set('views', __dirname + '/public');
+	app.set('view options', {layout: false});
+	app.set('basepath',__dirname + '/public');
 });
 
 app.configure('development', function(){
