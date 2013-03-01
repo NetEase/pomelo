8b2e1773443e4e6089b06d66ca71a37dc5d4f775
py <pengyang238@gmail.com>
2013-03-01 16:08:04 +0800

    update template
    Change-Id: I0bfc40951ebe546cf6e154f69d4d3c1c32117c2a

--- template/game-server/app.js
+++ template/game-server/app.js
@@ -6,6 +6,17 @@ var pomelo = require('pomelo');
 var app = pomelo.createApp();
 app.set('name', '$');
 
+// app configuration
+app.configure('production|development', 'connector', function(){
+	app.set('connectorConfig',
+		{
+			connector : pomelo.connectors.hybridconnector,
+			heartbeat : 3,
+			useDict : true,
+			useProtobuf : true
+		});
+});
+
 // start app
 app.start();
 
