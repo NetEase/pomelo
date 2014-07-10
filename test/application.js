74513a73cce8a46696630318147acfe714416aae
py8765 <pengyang633@126.com>
2014-07-10 17:42:40 +0800

    upgrade to 1.0.2
    Change-Id: I1aa14f503d57b22a02a6b88a9e0a2c0b8bd121bc

--- test/application.js
+++ test/application.js
@@ -2,7 +2,7 @@ var app = require('../lib/application');
 var pomelo = require('../');
 var should = require('should');
 
-var WAIT_TIME = 300;
+var WAIT_TIME = 1000;
 var mockBase = process.cwd() + '/test';
 
 describe('application test', function(){
