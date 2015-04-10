NetEase/pomelo

commit 6b814864d3de70eec20a59bc966613a29143652e

tls: use switcher to manager tls server
this commit is proposed by PR #683,

https://github.com/NetEase/pomelo/pull/683

Thanks to @wuxian.

Change-Id: Ia2f51ac202aaa45b0427c8f39e493deb3635a1ca

lib/connectors/hybridconnector.js
@@ -7,9 +7,9 @@ var Handshake = require('./commands/handshake');
 var Heartbeat = require('./commands/heartbeat');
 var Kick = require('./commands/kick');
 var coder = require('./common/coder');
-var Tlssocket = require('./hybrid/tlssocket');
 var Message = require('pomelo-protocol').Message;
 var Constants = require('../util/constants');
+var tls = require('tls');
 var logger = require('pomelo-logger').getLogger('pomelo', __filename);
 
 var curId = 1;
@@ -64,34 +64,28 @@ Connector.prototype.start = function(cb) {
   this.decodeIO_protobuf = app.components.__decodeIO__protobuf__;
 
   if(!this.ssl) {
-    this.tcpServer = net.createServer();
-    this.switcher = new Switcher(this.tcpServer, self.opts);
-
-    this.switcher.on('connection', function(socket) {
-      gensocket(socket);
-    });
-
-    if(!!this.distinctHost) {
-      this.tcpServer.listen(this.port, this.host);
-    } else {
-      this.tcpServer.listen(this.port);
-    }
+    this.listeningServer = net.createServer();
   } else {
-    this.tlssocket = new Tlssocket(this.port, this.opts);
-    this.tlssocket.on('connection', function(socket) {
-      gensocket(socket);
-    });
+    this.listeningServer = tls.createServer(this.ssl);
   }
+  this.switcher = new Switcher(this.listeningServer, self.opts);
+
+  this.switcher.on('connection', function(socket) {
+    gensocket(socket);
+  });
+
+  if(!!this.distinctHost) {
+    this.listeningServer.listen(this.port, this.host);
+  } else {
+    this.listeningServer.listen(this.port);
+  }
+
   process.nextTick(cb);
 };
 
 Connector.prototype.stop = function(force, cb) {
-  if (!this.ssl) {
-    this.switcher.close();
-    this.tcpServer.close();
-  } else {
-    this.tlssocket.close();
-  }
+  this.switcher.close();
+  this.tcpServer.close();
 
   process.nextTick(cb);
 };

lib/connectors/hybrid/switcher.js
@@ -28,7 +28,12 @@ var Switcher = function(server, opts) {
   this.timeout = opts.timeout || DEFAULT_TIMEOUT;
   this.setNoDelay = opts.setNoDelay;
 
-  this.server.on('connection', this.newSocket.bind(this));
+  if (!opts.ssl) {
+    this.server.on('connection', this.newSocket.bind(this));
+  } else {
+    this.server.on('secureConnection', this.newSocket.bind(this));
+  }
+
   this.wsprocessor.on('connection', this.emit.bind(this, 'connection'));
   this.tcpprocessor.on('connection', this.emit.bind(this, 'connection'));
 
@@ -66,6 +71,7 @@ Switcher.prototype.newSocket = function(socket) {
       clearTimeout(self.timers[socket.id]);
       delete self.timers[socket.id];
     }
+    // FIXME: handle incomplete HTTP method
     if(isHttp(data)) {
       processHttp(self, self.wsprocessor, socket, data);
     } else {

lib/connectors/hybrid/tlssocket.js
@@ -1,44 +1,0 @@
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
-var https = require('https');
-var tls = require('tls');
-var utils = require('../../util/utils');
-var TcpSocket = require('./tcpsocket');
-var WebSocketServer	= require('ws').Server;
-var logger = require('pomelo-logger').getLogger('pomelo', __filename);
-
-var HEAD_SIZE = 4;
-
-var TLS = function(port, opts) {
-	EventEmitter.call(this);
-	var self = this;
-	var ssl = opts.ssl;
-	switch(ssl.type) {
-		case 'wss':
-			var httpsServer = https.createServer(ssl).listen(port);
-			this.server = new WebSocketServer({server: httpsServer});
-			this.server.on('connection', this.emit.bind(this, 'connection'));
-			break;
-			case 'tls':
-				this.server = tls.createServer(ssl, function(socket) {
-					var tcpsocket = new TcpSocket(socket, {headSize: HEAD_SIZE,
-						headHandler: utils.headHandler,
-						closeMethod: opts.closeMethod});
-					self.emit('connection', tcpsocket);
-				}).listen(port);	
-			break;
-			default:
-				logger.error('undefined ssl type: %s', ssl.type);
-			break;
-	}
-};
-
-util.inherits(TLS, EventEmitter);
-
-TLS.prototype.close = function() {
-	if (this.server) {
-		this.server.close();
-	}
-};
-
-module.exports = TLS;

commit d323083374242ce1dce327989d116189d9ad740b

hybrid: fix a typo introduced by 6b814864d3de70eec
Change-Id: Ibd29204b3ad44750d0c32481d4ac9b703f2a775a

lib/connectors/hybridconnector.js
@@ -83,7 +83,7 @@ Connector.prototype.start = function(cb) {
 
 Connector.prototype.stop = function(force, cb) {
   this.switcher.close();
-  this.tcpServer.close();
+  this.listeningServer.close();
 
   process.nextTick(cb);
 };