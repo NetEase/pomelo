e5570db06f5a2ca19620c842f8f4c6fa50c7f01e
iygnohz <iygnohz@gmail.com>
2015-11-25 12:16:08 +0800

    trailing whitespaces

--- template/web-server/public/js/lib/local/boot/index.js
+++ template/web-server/public/js/lib/local/boot/index.js
@@ -3,9 +3,9 @@
 
   var protocol = require('pomelo-protocol');
   window.Protocol = protocol;
-  
+
   var protobuf = require('pomelo-protobuf');
   window.protobuf = protobuf;
-  
+
   var pomelo = require('pomelo-jsclient-websocket');
   window.pomelo = pomelo;
